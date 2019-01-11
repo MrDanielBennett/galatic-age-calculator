78c1f1a77ab657fb460d96068b6046049ae2bae4
MrDanielBennett <daniel.bennett1999@gmail.com>
2019-01-11 15:29:13 -0800

    add life expectency for each planet that will say how much longer

--- src/main.js
+++ src/main.js
@@ -18,9 +18,9 @@ $(document).ready(function() {
     let result = user.age;
     let result2 = user.mercuryAge;
     let result3 = user.venusAge;
-    lifecheck();
     let result4 = user.marsAge;
     let result5 = user.jupiterAge;
+    lifecheck();
     $('#result').text("Earth: " + result + " years old, " + " Your Current life expecteny on this planet is: " + (lifeExpectency - result) + " years.");
     $('#result2').text("Mercury: " + result2 + " years old, " + " Your Current life expecteny on this planet is: " + (lifeExpectency - result2) + " years.");
     $('#result3').text("Venus: " + result3 + " years old, " + " Your Current life expecteny on this planet is: " + (lifeExpectency - result3) + " years.");
@@ -29,10 +29,18 @@ $(document).ready(function() {
 
     function lifecheck(){
       if (lifeExpectency - result < 0) {
-        $('#liferesult').text("hey looks like you've beat the odds, good job. You've lived longer than the average person here by " + (result - lifeExpectency) + " year(s)")
-      } else if (lifeExpectency - result2 < 0){
-        $('#liferesult2').text("hey looks like you've beat the odds, good job. You've lived longer than the average person here by " + (result2 - lifeExpectency) + " year(s)")
-
+        $('#liferesult').text("hey looks like you've beat the odds, good job. You've lived longer than the average person here by " + (result - lifeExpectency) + " year(s)");
+      }
+      if (lifeExpectency - result2 < 0){
+        $('#liferesult2').text("hey looks like you've beat the odds, good job. You've lived longer than the average person here by " + (result2 - lifeExpectency) + " year(s)");}
+      if (lifeExpectency - result3 < 0){
+        $('#liferesult3').text("hey looks like you've beat the odds, good job. You've lived longer than the average person here by " + (result3 - lifeExpectency) + " year(s)");
+      }
+        if (lifeExpectency - result4 < 0){
+        $('#liferesult4').text("hey looks like you've beat the odds, good job. You've lived longer than the average person here by " + (result4 - lifeExpectency) + " year(s)");
+      }
+      if (lifeExpectency - result5 < 0){
+        $('#liferesult5').text("hey looks like you've beat the odds, good job. You've lived longer than the average person here by " + (result5 - lifeExpectency) + " year(s)");
       }
     }
 
