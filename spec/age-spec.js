3574a9530b6267bbd75aeea2c3425394cffaa8af
MrDanielBennett <daniel.bennett1999@gmail.com>
2019-01-11 14:07:16 -0800

    refactor specs to work with new function paths, change background and text color

--- spec/age-spec.js
+++ spec/age-spec.js
@@ -1,24 +1,32 @@
-// import { getAge } from ./../src/space-age.js
+import { Space_age } from './../src/space-age.js'
 
 describe ('Space_age', function() {
 
   it('should show the age of the user on Mercury', function(){
-    let user = new Space_age("1999/03/24");
-    expect(user.getMercuryAge()).toEqual(79);
+    let user = new Space_age('1999/03/24');
+    user.getAge();
+    user.getMercuryAge();
+    expect(user.mercuryAge).toEqual(79);
   });
 
   it('should show the age of the user on Venus', function(){
     let user = new Space_age("1999/03/24");
-    expect(user.getVenusAge()).toEqual(30);
+    user.getAge();
+    user.getVenusAge();
+    expect(user.venusAge).toEqual(30);
   });
 
   it('should show the age of the user on Mars', function(){
     let user = new Space_age("1999/03/24");
-    expect(user.getMarsAge()).toEqual(10);
+    user.getAge();
+    user.getMarsAge();
+    expect(user.marsAge).toEqual(10);
   });
 
   it('should show the age of the user on Jupiter', function(){
   let user = new Space_age("1999/03/24");
-  expect(user.getJupiterAge()).toEqual(79);
+  user.getAge();
+  user.getJupiterAge();
+  expect(user.jupiterAge).toEqual(1);
   });
 });
