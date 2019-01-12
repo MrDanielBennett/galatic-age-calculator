67d4dc49bc5936ad2d29ac69c3bdfcadb6f38ec3
MrDanielBennett <daniel.bennett1999@gmail.com>
2019-01-11 16:38:26 -0800

    add README.md as well as fix spellig error of expectancy throughout full project

---
+++ README.md
@@ -0,0 +1,52 @@
+# _Space Age Calculator_
+
+#### _A Javascript webpage that will return your age on different planets as well as how much longer you're expected to live on those planets._
+
+#### By _**Daniel Bennett**_
+
+## Description
+
+_Ever wonder how old you'd be in Mars years? Jupiter Years? How about Mercury years? This Javascript program is here to answer those questions, by inserting your date of birth "YYYY/MM/DD" the program will tell you how old you are on a number of different planets. Not only will it tell you how old you are but also how much longer you have to live there._
+
+### Specs
+* calculates how old you are on Mercury.
+      Input = birthday - "1999/03/24" life Expectancy -78
+      Output = 79 years old, and lived longer by 1 year.
+* calculates how old you are on Venus.
+      Input = birthday - "1999/03/24" life Expectancy -78
+      Output = 79 years old, current Expectancy 48 years.
+* calculates how old you are on Mars.
+      Input = birthday - "1999/03/24" life Expectancy -78
+      Output = 10 years old, current Expectancy 68 years.
+* calculates how old you are on Jupiter.
+      Input = birthday - "1999/03/24" life Expectancy -78
+      Output = 1 years old, current Expectancy 77 years.
+* calculates how old you are on Earth.
+      Input = birthday - "1999/03/24" life Expectancy -78
+      Output = 19 years old, current Expectancy 59 years.
+
+
+## Setup/Installation Requirements
+
+* Clone or Download from Github @ https://github.com/MrDanielBennett/galatic-age-calculator.git
+* In root of directory run "$ npm install"
+* Next run "$ npm run build"
+* Next run "$ npm run start" this will open up a window on http://localhost:8080/
+* To work program insert birthday as "YYYY/DD/MM" and the life expectancy, then hit submit.
+
+## Known Bugs
+
+_No known bugs as of 01/11/19_
+
+## Support and contact details
+
+_If any issues or bugs are found please contact me @ daniel.bennett1999@gmail.com or on github @MrDanielBennett_
+
+## Technologies Used
+
+_Bootstrap paired with HTML, Javascript, CSS, and Webpack, Karma, Jasmine for full list check out package.json in project
+### License
+
+*MIT*
+
+Copyright (c) 2019 **_Daniel Bennett_**
--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
     event.preventDefault();
     let birthday = $('#birthday-input').val();
     let user = new Space_age(birthday);
-    const lifeExpectency = $('#lifeExpectency-input').val();
+    const lifeExpectancy = $('#lifeExpectancy-input').val();
     user.getAge();
     user.getMercuryAge();
     user.getVenusAge();
@@ -21,27 +21,27 @@ $(document).ready(function() {
     let result4 = user.marsAge;
     let result5 = user.jupiterAge;
     lifecheck();
-    $('#result').text("Earth: " + result + " years old, " + " Your Current life expecteny on this planet is: " + (lifeExpectency - result) + " years.");
-    $('#result2').text("Mercury: " + result2 + " years old, " + " Your Current life expecteny on this planet is: " + (lifeExpectency - result2) + " years.");
-    $('#result3').text("Venus: " + result3 + " years old, " + " Your Current life expecteny on this planet is: " + (lifeExpectency - result3) + " years.");
-    $('#result4').text("Mars: " + result4 + " years old, " + " Your Current life expecteny on this planet is: " + (lifeExpectency - result4) + " years.");
-    $('#result5').text("Jupiter: " + result5 + " years old, " + " Your Current life expecteny on this planet is: " + (lifeExpectency - result5) + " years.");
+    $('#result').text("Earth: " + result + " years old, " + " Your Current life expectancy on this planet is: " + (lifeExpectancy - result) + " years.");
+    $('#result2').text("Mercury: " + result2 + " years old, " + " Your Current life expectancy on this planet is: " + (lifeExpectancy - result2) + " years.");
+    $('#result3').text("Venus: " + result3 + " years old, " + " Your Current life expectancy on this planet is: " + (lifeExpectancy - result3) + " years.");
+    $('#result4').text("Mars: " + result4 + " years old, " + " Your Current life expectancy on this planet is: " + (lifeExpectancy - result4) + " years.");
+    $('#result5').text("Jupiter: " + result5 + " years old, " + " Your Current life expectancy on this planet is: " + (lifeExpectancy - result5) + " years.");
 
     function lifecheck(){
-      if (lifeExpectency - result < 0) {
-        $('#liferesult').text("hey looks like you've beat the odds, good job. You've lived longer than the average person here by " + (result - lifeExpectency) + " year(s)");
+      if (lifeExpectancy - result < 0) {
+        $('#liferesult').text("hey looks like you've beat the odds, good job. You've lived longer than the average person here by " + (result - lifeExpectancy) + " year(s)");
       }
-      if (lifeExpectency - result2 < 0){
-        $('#liferesult2').text("hey looks like you've beat the odds, good job. You've lived longer than the average person here by " + (result2 - lifeExpectency) + " year(s)");
+      if (lifeExpectancy - result2 < 0){
+        $('#liferesult2').text("hey looks like you've beat the odds, good job. You've lived longer than the average person here by " + (result2 - lifeExpectancy) + " year(s)");
       }
-      if (lifeExpectency - result3 < 0){
-        $('#liferesult3').text("hey looks like you've beat the odds, good job. You've lived longer than the average person here by " + (result3 - lifeExpectency) + " year(s)");
+      if (lifeExpectancy - result3 < 0){
+        $('#liferesult3').text("hey looks like you've beat the odds, good job. You've lived longer than the average person here by " + (result3 - lifeExpectancy) + " year(s)");
       }
-      if (lifeExpectency - result4 < 0){
-        $('#liferesult4').text("hey looks like you've beat the odds, good job. You've lived longer than the average person here by " + (result4 - lifeExpectency) + " year(s)");
+      if (lifeExpectancy - result4 < 0){
+        $('#liferesult4').text("hey looks like you've beat the odds, good job. You've lived longer than the average person here by " + (result4 - lifeExpectancy) + " year(s)");
       }
-      if (lifeExpectency - result5 < 0){
-        $('#liferesult5').text("hey looks like you've beat the odds, good job. You've lived longer than the average person here by " + (result5 - lifeExpectency) + " year(s)");
+      if (lifeExpectancy - result5 < 0){
+        $('#liferesult5').text("hey looks like you've beat the odds, good job. You've lived longer than the average person here by " + (result5 - lifeExpectancy) + " year(s)");
       }
     }
 
